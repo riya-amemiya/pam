1c366c724b075f9126091e5331ffa18d51b5c8bc
Riya Amemiya <riyaamemiya@gmail.com>
2023-05-06 16:50:35 +0900

    ちょっと更新

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,16 @@
 module.exports = {
 	siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
 	generateRobotsTxt: true,
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: "*",
+				disallow: ["/dashboard/*"],
+			},
+			{
+				userAgent: "*",
+				allow: "/",
+			},
+		],
+	},
 };
