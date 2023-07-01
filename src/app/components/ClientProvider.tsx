9bcdc7bcef155a325bde0c8597936e2e70931ef7
Riya Amemiya <riyaamemiya@gmail.com>
2023-07-01 22:06:17 +0900

    feat(ClientProvider): セッション情報を受け取るためのprops追加

--- src/app/components/ClientProvider.tsx
+++ src/app/components/ClientProvider.tsx
@@ -2,17 +2,20 @@
 import { darkTheme, lightTheme } from "@/lib/themes";
 import { ThemeProvider } from "@emotion/react";
 import { useMediaQuery } from "@mui/material";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
 
 export const ClientProvider = ({
   children,
+  session,
 }: {
   children: React.ReactNode;
+  session?: Session | null;
 }) => {
   const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <RecoilRoot>
         <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
           {children}
---
+++ src/app/dashboard/client.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { SetSNSAccountReq, SetSNSAccountRes } from "types/prisma";
+import useSWRMutation from "swr/mutation";
+import { fetcherPost } from "@/lib/fetcherPost";
+import TextField from "@mui/material/TextField";
+import { Button } from "@/stories/Button";
+import Avatar from "@mui/material/Avatar";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { useSWRConfig } from "swr";
+import { GetUserDataRes } from "types/prisma/getUserDataType";
+export const DashboardClient = ({
+  data: userData,
+}: { data: GetUserDataRes }) => {
+  const { trigger: setSNSAccount } = useSWRMutation(
+    "/api/prisma/setSNSAccount",
+    fetcherPost<SetSNSAccountReq, SetSNSAccountRes>,
+  );
+  const { mutate: mutateGetUserData } = useSWRConfig();
+  console.log("====================================");
+  console.log(userData);
+  console.log("====================================");
+  return (
+    <div>
+      <div className="flex items-center">
+        <div>
+          <Avatar
+            src={userData.user?.image as string}
+            sx={{
+              width: 100,
+              height: 100,
+            }}
+          />
+        </div>
+        <div>
+          <h1 className="text-4xl">{userData.user?.name}</h1>
+        </div>
+      </div>
+
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const target = e.target as typeof e.target & {
+            GitHub?: { value: string };
+          };
+          await setSNSAccount({
+            GitHubLink: target.GitHub?.value || "",
+          });
+          await mutateGetUserData("/api/prisma/getUserData");
+        }}
+      >
+        <div>
+          <div className="flex justify-center items-center">
+            {userData && userData?.statusCode === 200 && (
+              <TextField
+                className="text-blue-500"
+                defaultValue={userData.user?.GitHub || ""}
+                name="GitHub"
+                placeholder="GitHub Account Name"
+                type="text"
+              />
+            )}
+            <a
+              href={`https://github.com/${userData.user?.GitHub}`}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <GitHubIcon
+                sx={{
+                  fontSize: 50,
+                }}
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <Button size="large" type="submit">
+            Submit
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
