01e10de8453d7bc858887aec348d4192ba93e83f
Riya Amemiya <riyaamemiya@gmail.com>
2023-07-30 17:32:39 +0900

    feat(sitemap): サイトマップにニュースURLを追加

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,14 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const url = process.env.NEXT_PUBLIC_SITE_URL as string;
   return [
     {
-      url: process.env.NEXT_PUBLIC_SITE_URL as string,
+      url,
+      lastModified: new Date(),
+    },
+    {
+      url: `${url}/news`,
       lastModified: new Date(),
     },
   ];
